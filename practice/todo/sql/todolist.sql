3de2f1c5d1d5bb31704de27d3631de3661ca8ea8
muramura-cloud <clu363721@gmail.com>
2020-09-21 12:43:17 +0900

    delete fir

--- practice/todo/sql/todolist.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `todolist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `content` text NOT NULL,
-  `flg` int(11) NOT NULL COMMENT '1の時は実行中、0の時は終了',
-  KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
